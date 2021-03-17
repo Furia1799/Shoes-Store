56585ff71e3c7123236e750f106148f1b4e6ed51
Bryan Reyes <bryan.reyes6408@alumnos.udg.mx>
2021-03-17 14:59:11 -0600

    Primer commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shoe'store | Bienvenido </title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div id= "titulo" class="">
+                <img src="" alt="Store">
+                <h1>SHOE'STORE</h1>
+            </div>
+            <div id="barra_navegacion">
+                <nav>
+                    <a id="logo"href="#">
+                        <img src="">
+                    </a>
+                    <ul>
+                        <il>
+                            <a>Mujeres</a>
+                        </il>
+                        <il>
+                            <a>Hombres</a>
+                        </il>
+                        <il>
+                            <a>Novedades</a>
+                        </il>
+                        <il>
+                            <a>Nosotros</a>
+                        </il>
+                    </ul>
+                    <a>
+                        <button>Regisrate</button>
+                    </a>
+                    <a>
+                        <button>Iniciar Sesion</button>
+                    </a>
+                    <a>
+                        <button>Salir</button>
+                    </a>
+                </nav>
+            </div>
+
+        </div>
+    </header>
+    <section>
+        <div class="container">
+            <div id="Productos">
+                <h1>Productos</h1>
+                <div id="prodcuto">
+                    <a href="producto">
+                        <img src="" alt="">
+                        <h2>zapatoos negros</h2>
+                        <p> 300 pesos</p>
+                    </a>
+                </div>
+                <div id="prodcuto">
+                    <a href="producto">
+                        <img src="" alt="">
+                        <h2>zapatoos negros</h2>
+                        <p> 300 pesos</p>
+                    </a>
+                </div>
+                <div id="prodcuto">
+                    <a href="producto">
+                        <img src="" alt="">
+                        <h2>zapatoos negros</h2>
+                        <p> 300 pesos</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer id="footer">
+        <p>Desarrollado por Bryan Reyes y Alberto Ruelas </p>
+    </footer>
+</body>
+</html>
