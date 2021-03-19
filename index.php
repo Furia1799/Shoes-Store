e1aa66e127c309cf3664e2d920b34c4778066e25
Bryan Reyes <furia1799@gmail.com>
2021-03-19 00:04:08 -0600

    Primera entrega de proyecto front-end

--- index.php
+++ index.php
@@ -14,12 +14,19 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <header>
-        <div id="titulo" class="jumbotron jumbotron-fluid text-center">
-            <h1 class="display-4">SHOE'STORE</h1>
-            <img src="img/Logo.jpg" width="100" height="100">
+        <div id="titulo" class="jumbotron jumbotron-fluid ">
+            <div class="row justify-content-center">
+                <div class="col-lg-4 text-center">
+                    <h1 id="titulo_palabra" class="display-4">SHOE'STORE</h1>
+                </div>
+                <div class="col-lg-4 text-center" >
+                    <img src="img/Logo.jpg" width="90" height="80">
+                </div>
+            </div>
         </div>
     </header>
     <section>
@@ -30,13 +37,13 @@
             </a>
             <ul class="navbar-nav ">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Mujere</a>
+                    <a class="nav-link" href="php/mujeres.php">Mujere</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Hombre</a>
+                    <a class="nav-link" href="php/hombres.php">Hombre</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Novedades</a>
+                    <a class="nav-link" href="index.php">Novedades</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Nosotros</a>
@@ -44,12 +51,12 @@
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="login.php">
+                    <a class="nav-link" href="php/registro.php">
                         <button type="button" class="btn btn-info">Registrar</button>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link">
+                    <a class="nav-link" href="php/login.php">
                         <button type="button" class="btn btn-success">Iniciar Sesion</button>
                     </a>
                 </li>
@@ -60,108 +67,85 @@
                 </li>
             </ul>
         </nav>
-        <div class="container-fluid"><br>
-            <p>Some example text. Some example text. Some example text. Some example text. Some example text.</p>
-            <p>Some example text. Some example text. Some example text. Some example text. Some example text.</p>
-            <p>Some example text. Some example text. Some example text. Some example text. Some example text.</p>
-            <p>Some example text. Some example text. Some example text. Some example text. Some example text.</p>
-        </div>
         <div class="container-fluid">
-            <div class="row">
+            <div  class="row">
                 <div class="col-lg-12">
-                    <h1 class="display-4 text-center">Productos para Hombres</h1>
+                    <h1 id="productos_titulo" class="display-4 text-center">Productos para Novedades</h1>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-4">
+            <div  class="row">
+                <div id="imagen_prodcuto" class="col-lg-4">
                     <a href="producto ">
-                        <img src="img/tennis_1.jpg" class="rounded" alt="tennis_3" width="300" height="300">
-                        <h2>zapatoos negros</h2>
-                        <p> 300 pesos</p>
+                        <img class="imagen_producto" src="img/tennis_1.jpg" class="rounded" alt="tennis_3" width="300" height="300">
+                        <h2 class="titulo_producto"> zapatoos negros</h2>
+                        <p class="precio_producto"> $ 300 pesos</p>
                     </a>
                 </div>
-                <div class="col-lg-4">
+                <div  id="imagen_prodcuto" class="col-lg-4">
                     <a href="producto">
-                        <img src="img/tennis_2.jpg" class="rounded" alt="tennis_2" width="300" height="300">
-                        <h2>zapatoos negros</h2>
-                        <p> 300 pesos</p>
+                        <img class="imagen_producto" src="img/tennis_2.jpg" class="rounded" alt="tennis_2" width="300" height="300">
+                        <h2 class="titulo_producto" >zapatoos negros</h2>
+                        <p class="precio_producto"> $ 300 pesos</p>
                     </a>
                 </div>
-                <div class="col-lg-4">
+                <div  id="imagen_prodcuto" class="col-lg-4">
                     <a href="producto">
-                        <img src="img/tennis_3.jpg" class="rounded" alt="tennis_1" width="300" height="300">
-                        <h2>zapatoos negros</h2>
-                        <p> 300 pesos</p>
+                        <img class="imagen_producto" src="img/tennis_3.jpg" class="rounded" alt="tennis_1" width="300" height="300">
+                        <h2 class="titulo_producto" >zapatoos negros</h2>
+                        <p class="precio_producto"> $ 300 pesos</p>
                     </a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-4">
+            <div  class="row">
+                <div id="imagen_prodcuto" class="col-lg-4">
                     <a href="producto ">
-                        <img src="img/tennis_1.jpg" alt="tennis_3" width="300" height="300">
-                        <h2>zapatoos negros</h2>
-                        <p> 300 pesos</p>
+                        <img class="imagen_producto" src="img/tennis_1.jpg" class="rounded" alt="tennis_3" width="300" height="300">
+                        <h2 class="titulo_producto"> zapatoos negros</h2>
+                        <p class="precio_producto"> $ 300 pesos</p>
                     </a>
                 </div>
-                <div class="col-lg-4">
+                <div  id="imagen_prodcuto" class="col-lg-4">
                     <a href="producto">
-                        <img src="img/tennis_2.jpg" alt="tennis_2" width="300" height="300">
-                        <h2>zapatoos negros</h2>
-                        <p> 300 pesos</p>
+                        <img class="imagen_producto" src="img/tennis_2.jpg" class="rounded" alt="tennis_2" width="300" height="300">
+                        <h2 class="titulo_producto" >zapatoos negros</h2>
+                        <p class="precio_producto"> $ 300 pesos</p>
                     </a>
                 </div>
-                <div class="col-lg-4">
+                <div  id="imagen_prodcuto" class="col-lg-4">
                     <a href="producto">
-                        <img src="img/tennis_3.jpg" alt="tennis_1" width="300" height="300">
-                        <h2>zapatoos negros</h2>
-                        <p> 300 pesos</p>
+                        <img class="imagen_producto" src="img/tennis_3.jpg" class="rounded" alt="tennis_1" width="300" height="300">
+                        <h2 class="titulo_producto" >zapatoos negros</h2>
+                        <p class="precio_producto"> $ 300 pesos</p>
                     </a>
                 </div>
             </div>
-            <div>
-                <p>
-                    saegdsddddgafdgffdagdshdsjhfdsan
-                    <br>
-                    dsajfdbjsabdfnas
-                    dsajbfdsaf
-                    dsajfhjdnm
-                    sdkfkdsnfm
-                    <br>
-                    sdmfdsnmfds
-                    dsjfjdsnmf
-                    dfjdshkjfhjdsfn
-                    <br>
-                    dsjfhkjdsfds
-                    dsjfhjdshfs
-                    <br>
-                    dskjfkjdsf
-                    dskjdfhjds
-                    <br>
-                    sdjhfjsd
-                    dsjhfkldskflds
-                    dslkfjklds
-                    <br>
-                    dskfjksdj
-                    dlkjfkds
-                    <br>
-                    dskjflksddsk
-                    sdlkfdhjdsnf
-                    <br>
-                    dsjfkdsl
-                    slkfdslkf
-                    <br>
-                    dskfldsj
-                    sdkjf;lds
-                    <br>
-                    dskgjlkds
-                    dsjglkfds
-                    fdljgd
-                </p>
+            <div  class="row">
+                <div id="imagen_prodcuto" class="col-lg-4">
+                    <a href="producto ">
+                        <img class="imagen_producto" src="img/tennis_1.jpg" class="rounded" alt="tennis_3" width="300" height="300">
+                        <h2 class="titulo_producto"> zapatoos negros</h2>
+                        <p class="precio_producto"> $ 300 pesos</p>
+                    </a>
+                </div>
+                <div  id="imagen_prodcuto" class="col-lg-4">
+                    <a href="producto">
+                        <img class="imagen_producto" src="img/tennis_2.jpg" class="rounded" alt="tennis_2" width="300" height="300">
+                        <h2 class="titulo_producto" >zapatoos negros</h2>
+                        <p class="precio_producto"> $ 300 pesos</p>
+                    </a>
+                </div>
+                <div  id="imagen_prodcuto" class="col-lg-4">
+                    <a href="producto">
+                        <img class="imagen_producto" src="img/tennis_3.jpg" class="rounded" alt="tennis_1" width="300" height="300">
+                        <h2 class="titulo_producto" >zapatoos negros</h2>
+                        <p class="precio_producto"> $ 300 pesos</p>
+                    </a>
+                </div>
             </div>
         </div>
     </section>
     <footer id="footer">
-        <p>Desarrollado por Bryan Reyes y Alberto Ruelas <?= date('Y') ?></p>
+        <p>Desarrollado por Bryan Reyes y Alberto Ruelas <?=date('Y')?></p>
     </footer>
 </body>
 </html>
